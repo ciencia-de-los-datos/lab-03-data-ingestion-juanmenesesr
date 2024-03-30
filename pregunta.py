a1dc945a571bcfbc3c11c7de60bd48da0797b2b9
juanmenesesr <jmenesesr@unal.edu.co>
2024-03-30 16:36:19 -0500

    Intento 1

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,44 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    with open("clusters_report.txt", "r") as file:
+        data = file.readlines()
+
+    data = [linea.strip() for linea in data]
+
+    data_slice = data[4:]
+    pattern = r'^\s*(\d+)\s+(\d+)\s+([\d.,]+)\s*%\s*(.*)'
+
+    full_data = []
+    lista = []
+    text = ""
+    for line in data_slice:
+        result = re.match(pattern, line)
+        if result:
+            lista = [int(result.group(1)), int(result.group(2)), float(result.group(3).replace(',', '.'))]
+            text = result.group(4)
+        else:
+            text += ' ' + line.strip()
+
+        if not line.strip():
+            lista.append(text.strip())
+            full_data.append(lista)
+            lista = []
+            text = ""
+
+    col = ["Cluster", "Cantidad de palabras clave", "Porcentaje de palabras clave", "Principales palabras clave"]
+    col = [column.lower().replace(" ", "_") for column in col]
+    df = pd.DataFrame(full_data, columns=col)
+
+    df["principales_palabras_clave"] = df["principales_palabras_clave"].str.replace(r"\.", "", regex=True)
+    df["principales_palabras_clave"] = df["principales_palabras_clave"].str.replace(r"\s+", " ", regex=True)
 
     return df
+
+# Llamada a la función para probarla
+(ingest_data())
+
